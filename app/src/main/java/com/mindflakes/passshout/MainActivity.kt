b4a82af6c73e46ebf18d1e098bf30b4c13758970
Nelson Chen <crazysim@gmail.com>
2019-02-26 22:35:28 -0800

    Open settings

--- app/src/main/java/com/mindflakes/passshout/MainActivity.kt
+++ app/src/main/java/com/mindflakes/passshout/MainActivity.kt
@@ -2,11 +2,16 @@ package com.mindflakes.passshout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.provider.Settings
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val intent = Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS)
+        startActivity(intent)
     }
 }
